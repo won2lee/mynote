f16f869cbe1027adb48e95f2ac55c7d123613d62
john lee <inno314159@gmail.com>
2021-02-12 21:30:12 +0900

    refactoring

--- index.py
+++ index.py
@@ -3,38 +3,46 @@ print("Content-Type: text/html")
 print()
 import cgi
 from linkdb import get_notelist, get_note
+from util import form_maker, js_css, gen_action
 
 # files = os.listdir('data')
 # listStr = ''
 # for item in files:
 #     listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
 
-def form_maker(clss,id,class1=None):
-    clsub = "confirm" if clss=="delete" else clss
-    scrpt = '''
-            <form action="{clss}.py" method="post">
-                <input type="hidden" name="id" value="{id}">
-                <input type="hidden" name="class1" value="{class1}">
-                <input type="submit" value="{clsub}">
-            </form>
-        '''.format(clss=clss,id=id,class1=class1,clsub=clsub)
-    return scrpt
+# def form_maker(clss,id,class1=None):
+#     clfunc = "confirm" if clss=="delete" else clss
+#     scrpt = '''
+#             <form action="{clfunc}.py" method="post">
+#                 <input type="hidden" name="id" value="{id}">
+#                 <input type="hidden" name="class1" value="{class1}">
+#                 <input type="submit" value="{clss}">
+#             </form>
+#         '''.format(clss=clss,id=id,class1=class1,clfunc=clfunc)
+#     return scrpt
 
 form = cgi.FieldStorage()
-clist = form["clist"].value if 'clist' in form else 0
-class1 = form["class1"].value if 'class1' in form else None
+# def gen_action(form):
+#     clist = form["clist"].value if 'clist' in form else 0
+#     class1 = form["class1"].value if 'class1' in form else None
+#     class2 = form["class2"].value if 'class2' in form else None
+#
+#     if 'id' in form:
+#         noteId = form["id"].value
+#         description = get_note(noteId)
+#         update_link = form_maker('update',noteId,class1,class2)
+#         #'<a href="update.py?id={}&class1={}">update</a>'.format(pageId,class1)
+#         delete_action = form_maker('delete',noteId)
+#     else:
+#         noteId = ''
+#         description = get_note()
+#         update_link = ''
+#         delete_action = ''
+#     create_link=form_maker('create',1)
+#
+#     return noteId,clist,class1,description,"{}{}{}".format(create_link,update_link,delete_action)
 
-if 'id' in form:
-    pageId = form["id"].value
-    description = get_note(pageId)
-    update_link = form_maker('update',pageId,class1)
-    #'<a href="update.py?id={}&class1={}">update</a>'.format(pageId,class1)
-    delete_action = form_maker('delete',pageId)
-else:
-    pageId = ''
-    description = get_note()
-    update_link = ''
-    delete_action = ''
+noteId,clist,class1,class2,description,bts = gen_action(form)
 #tl = '"'+pageId+'"'
 # descDB = readSQL(pageId)[0]['description'] if pageId!="Welcome" else ''
 print('''<!doctype html>
@@ -42,8 +50,9 @@ print('''<!doctype html>
 <head>
   <title>WEB1 - Welcome</title>
   <meta charset="utf-8">
-  <link type="text/css" rel="stylesheet" href="mystyle9.css">
-  <script src="colors.js"></script>
+  {js_css}
+  # <link type="text/css" rel="stylesheet" href="mystyle9.css">
+  # <script src="colors.js"></script>
 </head>
 <body>
   <h1><a href="index.py">My Note</a></h1>
@@ -57,16 +66,19 @@ print('''<!doctype html>
       <div id="article">
           <p>{desc}</p>
           <div id="to_action">
-            {create_link}{update_link}{delete_action}
+            {buttons}
           </div>
       </div>
   </div>
 </body>
 </html>
-'''.format(title=str(pageId),
+'''.format(title=str(noteId),
+            js_css = js_css(),
             desc=description,
             listStr=get_notelist(clist,class1),
-            create_link=form_maker('create',1),
-            update_link=update_link,
-            delete_action=delete_action
-            ))
+            buttons=bts))
+            # {create_link}{update_link}{delete_action}
+            # create_link=form_maker('create',1),
+            # update_link=update_link,
+            # delete_action=delete_action
+            # ))
--- update2.py
+++ update2.py
@@ -9,32 +9,19 @@ from linkdb import get_notelist, get_note
 # listStr = ''
 # for item in files:
 #     listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
-
-def form_maker(clss,id,class1=None):
-    scrpt = '''
-            <form action="{clss}.py" method="post">
-                <input type="hidden" name="id" value="{id}">
-                <input type="hidden" name="class1" value="{class1}">
-                <input type="submit" value="{clss}">
-            </form>
-        '''.format(clss=clss,id=id,class1=class1)
-    return scrpt
-
 form = cgi.FieldStorage()
 clist = form["clist"].value if 'clist' in form else 0
 class1 = form["class1"].value if 'class1' in form else None
 class2 = form["class2"].value if 'class2' in form else None
 if 'id' in form:
     pageId = form["id"].value
-    description = get_note(pageId)
+    description = get_note(pageId,update=True)
     #update_link = '<a href="update.py?id={}&class1={}">update</a>'.format(pageId,class1)
     #description = open('data/'+pageId, 'r').read()
-    delete_action = form_maker('delete',pageId)
 else:
     pageId = 'Welcome'
     description = get_note()
     #update_link = ''
-    delete_action = ""
 
 print('''<!doctype html>
 <html>
@@ -51,22 +38,18 @@ print('''<!doctype html>
     {listStr}
     </ol>
     <div id="article">
-        <p>
         <form action="process_update.py" method="get">
           <p><input type="hidden" name="id" value={id}>
              <input type="text" name="class1" value='{class1}'>
              <input type="text" name="class2" placeholder='{class2}'>
           </p>
-          <p><textarea row="10" type="text" style="width:80%;" name="note">{note}</textarea></p>
+          <p><textarea class="autosize" row="10" type="text" style="width:80%;" name="note">{note}</textarea></p>
+          <p><input type="submit"></p>
         </form>
-        </p>
-        <div id="to_action2">
-        "Are you sure to confirm deleting" {delete_action}
-        </div>
     </div>
   </div>
 </body>
 </html>
 '''.format(id=pageId,listStr=get_notelist(clist,class1),
-            class1=class1,class2=class2,note=description,delete_action=delete_action
+            class1=class1,class2=class2,note=description
             ))
